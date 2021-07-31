4553c368538a8d984ed8bc98d9dca53f9b2d456a
Stepan Furman <sfurman@detmir.ru>
2021-07-31 09:19:46 +0300

    added BooksApiModule

--- app/src/main/java/ru/furman/booksexplorer/network/BooksApiModule.kt
+++ app/src/main/java/ru/furman/booksexplorer/network/BooksApiModule.kt
@@ -1,12 +1,33 @@
 package ru.furman.booksexplorer.network
 
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class BooksApiModule {
-    
+
+    @Provides
+    fun provideBooksApi(okHttpClient: OkHttpClient): BooksApi {
+        val responseGson = GsonBuilder().create()
+        return Retrofit.Builder()
+            .baseUrl(API_DOMAIN)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(responseGson))
+            .build()
+            .create(BooksApi::class.java)
+    }
+
+    companion object {
+
+        private const val API_DOMAIN = "https://fakerapi.it/api"
+
+    }
 
 }
