bb4c529b06cd59accaccee34ceec9cbd0123aed1
Stepan Furman <sfurman@detmir.ru>
2021-08-03 12:21:24 +0300

    added DetailsScreenPreview

--- app/src/main/java/ru/furman/booksexplorer/ui/details/DetailsScreen.kt
+++ app/src/main/java/ru/furman/booksexplorer/ui/details/DetailsScreen.kt
@@ -4,16 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.*
 import kotlinx.coroutines.flow.collect
@@ -28,40 +26,51 @@ import ru.furman.booksexplorer.viewmodel.details.BookDetailsViewModel
 @Composable
 @ExperimentalPagerApi
 fun DetailsScreen(viewModel: BookDetailsViewModel) {
-    StatesOf(viewModel = viewModel) { state, effect ->
+    StatesOf(viewModel = viewModel) { state, _ ->
         BooksExplorerTheme {
-            Column(Modifier.fillMaxSize()) {
-                Toolbar(title = state.toolbarTitle)
-                val pagerState = rememberPagerState(pageCount = 2)
-
-                val onPageSelected = { page: Int ->
+            Content(
+                state = state,
+                onPageSelected = { page ->
                     viewModel.handleEvent(BookDetailsUiEvent.PageSelected(page))
                 }
+            )
+        }
+    }
+}
 
-                LaunchedEffect(pagerState) {
-                    snapshotFlow { pagerState.currentPage }.collect { onPageSelected(it) }
-                }
-                Tabs(state = state, pagerState = pagerState, onPageSelected = onPageSelected)
-                HorizontalPager(
-                    modifier = Modifier.fillMaxSize(),
-                    state = pagerState,
-                    verticalAlignment = Alignment.Top
-                ) { page ->
-                    if (page == 0) {
-                        DetailsFirstPageScreen(Modifier.padding(16.dp), state.firstPage)
-                    } else {
-                        DetailsSecondPageScreen(
-                            Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth(),
-                            state.secondPage
-                        )
-                    }
-                }
+@Composable
+@ExperimentalPagerApi
+private fun Content(
+    state: BookDetailsUiState,
+    onPageSelected: (Int) -> Unit
+) {
+    Column(Modifier.fillMaxSize()) {
+        Toolbar(title = state.toolbarTitle)
+        val pagerState = rememberPagerState(pageCount = 2)
+
+        LaunchedEffect(pagerState) {
+            snapshotFlow { pagerState.currentPage }.collect { onPageSelected(it) }
+        }
+        Tabs(state = state, pagerState = pagerState, onPageSelected = onPageSelected)
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { page ->
+            if (page == 0) {
+                DetailsFirstPageScreen(Modifier.padding(16.dp), state.firstPage)
+            } else {
+                DetailsSecondPageScreen(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    state.secondPage
+                )
             }
         }
     }
 }
+
 
 @Composable
 @ExperimentalPagerApi
@@ -86,5 +95,34 @@ private fun Tabs(state: BookDetailsUiState, pagerState: PagerState, onPageSelect
             selected = !state.isFirstPageSelected,
             onClick = { onPageSelected(1) },
         )
+    }
+}
+
+@Preview
+@Composable
+@ExperimentalPagerApi
+private fun DetailsScreenPreview() {
+    Surface {
+        BooksExplorerTheme {
+            Content(
+                state = BookDetailsUiState(
+                    toolbarTitle = "Book's name",
+                    firstPage = BookDetailsUiState.FistPage(
+                        title = "Evgeniy Onegin",
+                        author = "Pushkin",
+                        genre = "Roman",
+                        description = "Cool cool cool Cool cool cool Cool cool cool Cool cool cool Cool cool cool Cool cool cool Cool cool cool Cool cool cool",
+                        imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.labirint.ru%2Fbooks%2F669673%2F&psig=AOvVaw2Lx0h1Dmze0XREl4AtvWWu&ust=1627889904166000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLDo9M6oj_ICFQAAAAAdAAAAABAD",
+                    ),
+                    secondPage = BookDetailsUiState.SecondPage(
+                        isbn = "21412523534543",
+                        publishedDate = "28.06.1996",
+                        publisher = "Piter"
+                    ),
+                    isFirstPageSelected = true
+                ),
+                onPageSelected = {}
+            )
+        }
     }
 }
