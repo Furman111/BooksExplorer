6e7d898922568c90e3bbfc3f553ba4b4ee5d93d1
Stepan Furman <sfurman@detmir.ru>
2021-08-09 15:44:38 +0300

    added BuyButton

---
+++ app/src/main/java/ru/furman/booksexplorer/ui/component/BuyButton.kt
@@ -0,0 +1,46 @@
+package ru.furman.booksexplorer.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.furman.booksexplorer.R
+
+@Composable
+fun BuyButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Text(
+            text = stringResource(id = R.string.book_details_buy_button),
+            style = MaterialTheme.typography.button.copy(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BuyButtonPreview() {
+    Box(
+        Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        BuyButton(Modifier.fillMaxWidth()) {}
+    }
+}
